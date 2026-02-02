3e76fcb22bde0daf785fa37110384cd7f7b5a5f5
LeonelM21 <123224648+LeonelM21@users.noreply.github.com>
2026-02-02 11:22:34 -0500

    Update response text for Valentine's message

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ noBtn.addEventListener('click', () => {
 
 
 yesBtn.addEventListener('click', () => {
-    responseText.textContent = 'Great choice pookie!!! 💖 💞';
+    responseText.textContent = 'Great choice pookie!!! 💖 💞 You are my Valentine';
     gif.src = 'https://i.pinimg.com/originals/b4/65/34/b46534530b0ef3ffac6636f068dd2e12.gif';
     yesBtn.style.display = 'none';
     noBtn.style.display = 'none';
